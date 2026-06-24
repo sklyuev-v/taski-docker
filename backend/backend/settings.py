7aa3e0eae94954d8c5c7a2698db5e78fd60c79d1
Sklyuev Viktor <sklyevv@yandex.ru>
2026-06-24 23:15:29 +0300

    Fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.154.211.176', 'localhost', 'taskisklyevv.duckdns.org']
+ALLOWED_HOSTS = ['178.154.211.176', '127.0.0.1', 'localhost', 'taskisklyevv.duckdns.org']
 
 
 # Application definition
